9d2b308b511dfaca57d7b2f3a05f5fb2cff6a896
Fara-10 <haitova.farangis10@gmail.com>
2025-08-19 13:53:18 +0500

    fafas work is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily: {
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+       fontFamily: {
+        sans: ['Ubuntu', 'sans-serif'], // заменяем дефолтный sans
+      },
+    },
   },
        screens:{
       "mb":{max:"510px"}
